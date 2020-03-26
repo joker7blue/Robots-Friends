d606f69186067e83351a7ee9f2e11ede4670646c
joker7blue <legrandgeorges53@gmail.com>
2020-03-26 18:25:24 +0100

    Updates

---
+++ src/robotsData.js
@@ -0,0 +1,56 @@
+export const robots = [
+	
+	{
+		name: "Tangui Delor",
+		email: "tangui@gmail.com"
+	},
+	{
+		name: "John Doe",
+		email: "johndoe@gmail.com"
+	},
+	{
+		name: "Medard",
+		email: "medard@gmail.com"
+	},
+	{
+		name: "Gabrielle",
+		email: "gabira@gmail.com"
+	},
+	{
+		name: "Robert",
+		email: "robert@gmail.com"
+	},
+	{
+		name: "Emilie",
+		email: "emilie@gmail.com"
+	},
+	{
+		name: "Rosine",
+		email: "rosine@gmail.com"
+	},
+	{
+		name: "Elodie",
+		email: "elodie@gmail.com"
+	},
+	{
+		name: "Alfred",
+		email: "alfred@gmail.com"
+	}
+	,
+	{
+		name: "Georges",
+		email: "georges@gmail.com"
+	}
+	,
+	{
+		name: "Papi",
+		email: "papi@gmail.com"
+	}
+	,
+	{
+		name: "Mami",
+		email: "mami@gmail.com"
+	}
+];
+
+/* pass: delo12345 */
