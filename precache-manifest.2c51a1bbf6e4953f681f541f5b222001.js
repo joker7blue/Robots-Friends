7bacd055f9ceb9184629a4747b4ec931f1c8119e
joker7blue <legrandgeorges53@gmail.com>
2020-03-26 18:58:45 +0100

    Updates

---
+++ precache-manifest.2c51a1bbf6e4953f681f541f5b222001.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e9640eba6f46842087e79152a5f98f1",
+    "url": "/Robots-Friends/index.html"
+  },
+  {
+    "revision": "fd0801a93f02d50b5e7d",
+    "url": "/Robots-Friends/static/css/2.e40df323.chunk.css"
+  },
+  {
+    "revision": "7cf4bd897de4e35c82b7",
+    "url": "/Robots-Friends/static/css/main.9e5c6d7a.chunk.css"
+  },
+  {
+    "revision": "fd0801a93f02d50b5e7d",
+    "url": "/Robots-Friends/static/js/2.71e4d782.chunk.js"
+  },
+  {
+    "revision": "7cf4bd897de4e35c82b7",
+    "url": "/Robots-Friends/static/js/main.4751d874.chunk.js"
+  },
+  {
+    "revision": "cbd568d8547d507ab58d",
+    "url": "/Robots-Friends/static/js/runtime~main.5c0503dd.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Robots-Friends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
