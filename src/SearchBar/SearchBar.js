b63c0071cb5953554d145accfd22822dc0f0c243
joker7blue <46563349+joker7blue@users.noreply.github.com>
2021-01-17 19:45:59 +0100

    increase padding to search input

--- src/SearchBar/SearchBar.js
+++ src/SearchBar/SearchBar.js
@@ -6,7 +6,7 @@ const searchBar = (props) => {
 	return (
 
 		<form class="tc">
-		    <input id="name" class="input-reset ba b--black-20 pa3" placeholder="Search your robot friend" type="text" aria-describedby="name-desc" onChange={props.searchTyping}/>
+		    <input id="name" class="input-reset ba b--black-20 pa5" placeholder="Search your robot friend" type="text" aria-describedby="name-desc" onChange={props.searchTyping}/>
 		</form>
 	)
 }
